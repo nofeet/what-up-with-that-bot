1140ca68808b0c2f96ddeedbe28dc80a3b7746cf
Greg Nofi <nofinator@yahoo.com>
2013-01-28 17:15:48 -0500

    Make the timeout a constant. Fix interruption logic and misspelling.

--- bot.rb
+++ bot.rb
@@ -1,35 +1,41 @@
 require 'cinch'
 require 'time'
 
+# Reset interruption counter if this timeout (in seconds) is exceeded.
+INTERRUPTION_TIMEOUT = 30
+
 class WhatUpWithThat
   include Cinch::Plugin
 
   def initialize(*args)
     super
-    @num_interuptions = 0
-    @last_interuption_time = Time.new
+    @num_interruptions = 0
+    @last_interruption_time = Time.new
   end
 
   match /(.+)/, use_prefix: false
 
   def execute(m, message)
-    this_interuption_time = Time.new
-    if this_interuption_time - @last_interuption_time >= 30
-      # if it has been more than 30 seconds since last interuption, start over.
-      @num_interuptions = 0
+    this_interruption_time = Time.new
+    if this_interruption_time - @last_interruption_time >= INTERRUPTION_TIMEOUT
+      debug "Reseting interruption counter due to time out."
+      @num_interruptions = 0
     end
-    if @num_interuptions == 0 then
+
+    if @num_interruptions == 0 then
       m.reply "#{message}..."
-    elsif @num_interuptions == 1 then
+      @num_interruptions += 1
+    elsif @num_interruptions == 1 then
       m.reply "#{message}, yeah!"
+      @num_interruptions += 1
     else
       m.reply "#{message}, baby. And I've got to say...!"
       m.reply "Oooooo-eeeeee!"
       m.reply "What up with that? What's up with that?"
-      @num_interuptions = 0
+      @num_interruptions = 0
     end
-    @num_interuptions += 1
-    @last_interuption_time = Time.new
+
+    @last_interruption_time = Time.new
   end
 
 end
